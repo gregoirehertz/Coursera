fa3d62cb5ccdc117fcd1a624d78219fb3899432a
mohamadsolouki <mohamad.solouki@gmail.com>
2023-12-08 10:34:56 +0330

    Update train_model.py

--- model/train_model.py
+++ model/train_model.py
@@ -1,46 +1,65 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, roc_auc_score
 import joblib
-import os
+import logging
+import sys
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def train_model(data_path, model_save_path):
-    """
-    Train a fraud detection model.
-    Args:
-    - data_path: Path to the training data.
-    - model_save_path: Path to save the trained model.
-    """
-    # Load the dataset
-    data = pd.read_csv(data_path)
+def load_data(data_path):
+    try:
+        data = pd.read_csv(data_path)
+        logging.info(f"Data loaded successfully from {data_path}")
+        return data
+    except Exception as e:
+        logging.error(f"Error loading data: {e}")
+        sys.exit(1)
+
+
+def preprocess_data(data):
     X = data.drop('class', axis=1)
     y = data['class']
+    return X, y
 
-    # Split the data into training and testing sets
+
+def train_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
-    # Initialize and train the Random Forest Classifier
     model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
 
-    # Evaluate the model
-    y_pred = model.predict(X_test)
-    print(classification_report(y_test, y_pred))
+    # Hyperparameter tuning
+    param_grid = {'n_estimators': [100, 200], 'max_depth': [10, 20]}
+    grid_search = GridSearchCV(model, param_grid, cv=3, scoring='accuracy')
+    grid_search.fit(X_train, y_train)
 
-    # Save the trained model
-    joblib.dump(model, model_save_path)
+    best_model = grid_search.best_estimator_
+    logging.info(f"Best model parameters: {grid_search.best_params_}")
+
+    # Evaluation
+    y_pred = best_model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    roc_auc = roc_auc_score(y_test, y_pred)
+    logging.info(f"Model Accuracy: {accuracy}, ROC-AUC: {roc_auc}")
+
+    return best_model
 
 
-# Example usage
-if __name__ == "__main__":
-    data_dir = 'data/simulated_data'
-    model_dir = 'model/saved_models'
+def save_model(model, model_save_path):
+    try:
+        joblib.dump(model, model_save_path)
+        logging.info(f"Model saved to {model_save_path}")
+    except Exception as e:
+        logging.error(f"Error saving model: {e}")
 
-    # Train and save a model for each month's simulated data
-    for file in os.listdir(data_dir):
-        if file.endswith('.csv'):
-            month = file.split('_')[-1].split('.')[0]
-            print(f"Training model for month {month}...")
-            train_model(f"{data_dir}/{file}", f"{model_dir}/model_{month}.pkl")
+
+if __name__ == '__main__':
+    data_path = 'data/creditcard.csv'
+    model_save_path = 'model/saved_models/model.pkl'
+
+    data = load_data(data_path)
+    X, y = preprocess_data(data)
+    model = train_model(X, y)
+    save_model(model, model_save_path)
