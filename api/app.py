c924f79ceb49e7bb3407f447e7c16a7c54750964
mohamadsolouki <mohamad.solouki@gmail.com>
2023-12-08 09:59:25 +0330

    Update app.py

--- api/app.py
+++ api/app.py
@@ -5,21 +5,35 @@ import os
 
 app = Flask(__name__)
 
-# Load the latest trained model
-model_files = sorted([f for f in os.listdir('model/saved_models') if f.endswith('.pkl')])
-latest_model_file = model_files[-1]
-model = joblib.load(f'model/saved_models/{latest_model_file}')
+
+def load_model():
+    """Function to load the latest trained model."""
+    model_files = sorted([f for f in os.listdir('model/saved_models') if f.endswith('.pkl')])
+    latest_model_file = model_files[-1] if model_files else None
+    if latest_model_file:
+        return joblib.load(f'model/saved_models/{latest_model_file}')
+    else:
+        raise FileNotFoundError("No model file found in 'model/saved_models'.")
+
+
+model = load_model()
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    if not model:
+        return jsonify({'error': 'Model not loaded'}), 500
+
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+
     try:
-        data = request.json
-        df = pd.DataFrame(data, index=[0])
+        df = pd.DataFrame([data])
         prediction = model.predict_proba(df)[0][1]  # Probability of class 1 (fraud)
         return jsonify({'probability': prediction})
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return jsonify({'error': str(e)}), 500
 
 
 @app.route('/')
