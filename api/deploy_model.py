dc82d08374d344cf7d7077a331e188cc7244c4c7
mohamadsolouki <mohamad.solouki@gmail.com>
2023-12-08 21:28:27 +0330

    Update deploy_model.py

--- api/deploy_model.py
+++ api/deploy_model.py
@@ -1,39 +1,67 @@
 import os
 import requests
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def upload_model_to_server(model_path, server_url):
     """
     Upload the model to the server or cloud storage.
+
     Args:
-    - model_path: Path to the model file.
-    - server_url: URL of the server where the model needs to be uploaded.
+        model_path: Path to the model file.
+        server_url: URL of the server where the model needs to be uploaded.
+
+    Returns:
+        The response object from the server.
     """
-    files = {'file': open(model_path, 'rb')}
-    response = requests.post(server_url, files=files)
-    return response
+    try:
+        with open(model_path, 'rb') as model_file:
+            files = {'file': model_file}
+            response = requests.post(server_url, files=files)
+            response.raise_for_status()
+            return response
+    except requests.RequestException as e:
+        logging.error(f"Error uploading model to server: {e}")
+        raise
+    except IOError as e:
+        logging.error(f"Error opening model file: {e}")
+        raise
 
 
 def notify_server_to_reload_model(notification_url):
     """
     Notify the server to reload the model.
+
     Args:
-    - notification_url: URL to notify for model reload.
+        notification_url: URL to notify for model reload.
+
+    Returns:
+        The response object from the server.
     """
-    response = requests.post(notification_url)
-    return response
+    try:
+        response = requests.post(notification_url)
+        response.raise_for_status()
+        return response
+    except requests.RequestException as e:
+        logging.error(f"Error notifying server to reload model: {e}")
+        raise
 
 
-# Example usage
 if __name__ == "__main__":
-    latest_model_path = 'model/saved_models/model.pkl'
-    server_upload_url = 'http://127.0.0.1:5000/upload_model'
-    server_reload_url = 'http://127.0.0.1:5000/reload_model'
+    latest_model_path = os.getenv('LATEST_MODEL_PATH', 'model/saved_models/model.pkl')
+    server_upload_url = os.getenv('SERVER_UPLOAD_URL', 'http://127.0.0.1:5000/upload_model')
+    server_reload_url = os.getenv('SERVER_RELOAD_URL', 'http://127.0.0.1:5000/reload_model')
 
-    # Upload the model
-    upload_response = upload_model_to_server(latest_model_path, server_upload_url)
-    print(f"Model upload response: {upload_response.text}")
+    try:
+        # Upload the model
+        upload_response = upload_model_to_server(latest_model_path, server_upload_url)
+        logging.info(f"Model upload response: {upload_response.text}")
 
-    # Notify the server to reload the model
-    reload_response = notify_server_to_reload_model(server_reload_url)
-    print(f"Server reload response: {reload_response.text}")
+        # Notify the server to reload the model
+        reload_response = notify_server_to_reload_model(server_reload_url)
+        logging.info(f"Server reload response: {reload_response.text}")
+    except Exception as e:
+        logging.error(f"Deployment error: {e}")
